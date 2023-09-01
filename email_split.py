16cb8cb41c4ed310d4951b440b577465e617e3c4
markMintel <101613208+markMintel@users.noreply.github.com>
2023-09-01 13:19:52 -0400

    Update email_split.py

--- email_split.py
+++ email_split.py
@@ -5,28 +5,15 @@ case_4 = 'markmintel@gmail.com-Ringo@Dingo-Bingo'
 
 def pk_split(pk):
     split_1 = pk.split('@')
-    if len(split_1) == 2:
-        split_2 = split_1[1].split('-')
-        if len(split_2) == 2:
-            email = split_1[0] + '@' + split_2[0]
-            pet_name = split_2[1]
-        else:
-            email = split_1[0] + '@' + split_2[0]
-            pet_name = '-'.join(split_2[1:])
-    else:
-        corrected = '@'.join(split_1[1:])
-        split_2 = corrected.split('-')
-        if len(split_2) == 2:
-            email = split_1[0] + '@' + split_2[0]
-            pet_name = split_2[1]
-        else:
-            email = split_1[0] + '@' + split_2[0]
-            pet_name = '-'.join(split_2[1:])
-    
+    split_2 = split_1[1].split('-')
+
+    email = split_1[0] + '@' + split_2[0]
+    to_remove = email + '-'
+    pet_name = pk.replace(to_remove, '')
+
     print(f'email: {email}')
     print(f'pet name: {pet_name}')
     return
-        
 
 pk_split(case_1)
 pk_split(case_2)
